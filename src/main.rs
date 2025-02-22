9534d389afcec29c8e47626b2077467b61f0c4ce
PrethamMuthappa <prethambachira1819@gmail.com>
2025-02-22 20:41:46 +0530

    stretching the triangle

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use sdl2::event::Event;
 use sdl2::event::Event::MouseMotion;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+
 
 use triangle_test::Tri;
 use crate::triangle_test::Points;
@@ -26,6 +26,7 @@ fn main() {
         screen_height,
     ).position_centered().
         allow_highdpi()
+        .opengl()
         .build().expect("Error creating window ");
 
     let mut canvas=window.into_canvas().build().unwrap();
@@ -41,26 +42,27 @@ fn main() {
         canvas.set_draw_color(Color::RGB(0, 0, 0)); // Set black for clearing
         canvas.clear();
 
-        //
-        // canvas.set_draw_color(Color::RGB(255, 0, 0)); // Bright red line
-        //
-        // canvas.draw_line(
-        //     Point::new((screen_width / 2) as i32, (screen_height / 2) as i32),
-        //     Point::new((screen_width / 3) as i32, (screen_height / 3) as i32))
-        //     .expect("Failed to draw line");
-        //
-        //
-        //  canvas.draw_rect((Rect::new(120, 100, 200, 300))).expect("TODO: panic message");
 
-        let tri = Tri {
-            vertex1: Points { x: 100, y: 100 },
-            vertex2: Points { x: 200, y: 100 },
-            vertex3: Points { x: 150, y: 200 }
+        let mut tri = Tri {
+            vertex1: Points { x: 200, y: 200 },
+            vertex2: Points { x: 300, y: 200 },
+            vertex3: Points { x: 250, y: 300 }
         };
+
+        for event in event_pump.poll_iter(){
+            match event {
+                MouseMotion {x,y,..}=>{
+                    tri.vertex1.x=x;
+                    tri.vertex1.y=y;
+                },
+                _=>{}
+            }
+        }
 
         canvas.set_draw_color(Color::RGB(255, 0, 0)); // Red color
 
         tri.draw(&mut canvas).expect("Failed to draw triangle");
+
 
         for event in event_pump.poll_iter() {
             match event {
@@ -68,13 +70,14 @@ fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
-                Event::MouseMotion {x,y, .. } => {
+                MouseMotion {x,y, .. } => {
                     println!("x:{}",x);
                     println!("y {}",y);
                 },
                 _ => {}
             }
         }
+        
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
